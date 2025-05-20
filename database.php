3005ffed321e1669b977068501819582f4245ace
keerthivasan91 <keerthivasan1617@gmail.com>
2025-05-20 22:14:12 +0530

    Add contact information form to database.php

---
+++ database.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Information Form</title>
+</head>
+<body>
+    <h2>Contact Form</h2>
+    <form action="submit.php" method="post">
+        Name: <input type="text" name="name" required><br><br>
+        Address Line 1: <input type="text" name="address1" required><br><br>
+        Phonenumber: <input type="number" name="Phonenumber" required size="10"><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
